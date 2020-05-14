a3b01a180cc2df976ec90edf8e053ea703cf344e
John O'Neill <joneill_appleid@activedata.tech>
2020-05-14 19:29:22 +1000

    Boilerplate (#7)
    * Initial Commit
    
    * Initial Commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        },
+        user_password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        }
+    });
+
+    return Burger;
+};
